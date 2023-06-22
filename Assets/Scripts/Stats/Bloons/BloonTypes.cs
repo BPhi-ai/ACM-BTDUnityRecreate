1f86584f73513eb9d458dce9ac850e4150404982
BPhi-ai <brianpt1106@gmail.com>
2023-06-21 17:01:27 -0700

    Unity Upload into Github

---
+++ Assets/Scripts/Stats/Bloons/BloonTypes.cs
@@ -0,0 +1,21 @@
+namespace TMG.BloonsTD.Stats
+{
+    public enum BloonTypes
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Black,
+        White,
+        Tank,
+        TankPilot,
+        RedElite,
+        BluElite,
+        GreenElite,
+        YellowElite,
+        Gray,
+        Ghost,
+        FinalBoss
+    }
+}
